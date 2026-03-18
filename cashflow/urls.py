b387d85fe7fbe96c2008ae1937a3107bac30dd7a
PolinaIK <158270282+PolinaIK@users.noreply.github.com>
2026-03-18 16:09:21 +0700

    Update urls.py

--- cashflow/urls.py
+++ cashflow/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),                          # Админка
-    path('', include('core.urls')),                           # Твои основные страницы
-    path('accounts/', include('django.contrib.auth.urls')),  # Аутентификация: login/logout
+    path('admin/', admin.site.urls),                          
+    path('', include('core.urls')),                          
+    path('accounts/', include('django.contrib.auth.urls')),  
 ]
